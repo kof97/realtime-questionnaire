be2b6502f95182602b21039d6b338f1bd87cbd6c
kof97 <1048434786@qq.com>
2016-05-30 20:24:58 +0800

    添加mongodb

---
+++ models/poll.js
@@ -0,0 +1,11 @@
+    var mongoose = require('mongoose');
+    var voteSchema = new mongoose.Schema({ ip: 'String' });
+    var choiceSchema = new mongoose.Schema({ 
+        text: String,
+        votes: [voteSchema]
+    });
+    
+    exports.PollSchema = new mongoose.Schema({
+        question: { type: String, required: true },
+        choices: [choiceSchema]
+    });
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,59 @@
-var express = require('express');
-var router = express.Router();
+var mongoose = require('mongoose');
+var db = mongoose.createConnection('localhost', 'questionnaire');
+var PollSchema = require('../models/poll.js').PollSchema;
+var Poll = db.model('polls', PollSchema);
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Questionnaire' });
-});
+exports.index = function(req, res) {
+  	res.render('index', { title: 'Questionnaire' });
+};
 
-module.exports = router;
+// JSON API for list of polls
+exports.list = function(req, res) { 
+  	Poll.find({}, 'question', function(error, polls) {
+    	res.json(polls);
+  	});
+};
+
+// JSON API for getting a single poll
+exports.poll = function(req, res) {  
+  	var pollId = req.params.id;
+  	Poll.findById(pollId, '', { lean: true }, function(err, poll) {
+    	if(poll) {
+	      	var userVoted = false,
+	          	userChoice,
+	          	totalVotes = 0;
+	      	for(c in poll.choices) {
+	        	var choice = poll.choices[c]; 
+	        	for(v in choice.votes) {
+	          		var vote = choice.votes[v];
+	          		totalVotes++;
+		          	if(vote.ip === (req.header('x-forwarded-for') || req.ip)) {
+		            	userVoted = true;
+		            	userChoice = { _id: choice._id, text: choice.text };
+		          	}
+	        	}
+	      	}
+	      	poll.userVoted = userVoted;
+	      	poll.userChoice = userChoice;
+	      	poll.totalVotes = totalVotes;
+	      	res.json(poll);
+    	} else {
+      		res.json({error:true});
+    	}
+  	});
+};
+
+// JSON API for creating a new poll
+exports.create = function(req, res) {
+  	var reqBody = req.body,
+      	choices = reqBody.choices.filter(function(v) { return v.text != ''; }),
+      	pollObj = {question: reqBody.question, choices: choices};
+  	var poll = new Poll(pollObj);
+  	poll.save(function(err, doc) {
+    	if(err || !doc) {
+      		throw 'Error';
+    	} else {
+      		res.json(doc);
+    	}   
+  	});
+};
